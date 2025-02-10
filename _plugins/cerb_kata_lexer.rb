8cf6d27f142d4f85893c5af92d9a8badceb6383f
Jeff Standen <jeff@webgroupmedia.com>
2025-02-10 13:31:59 -0800

    [Code/Syntax] Converted code block syntax highlighting from Prism.js (client-side) to Rouge (server-side). This is faster client-side and supports more languages (since it doesn't need to export them). It also supports line numbering + line range highlighting.

---
+++ _plugins/cerb_kata_lexer.rb
@@ -0,0 +1,41 @@
+require 'rouge'
+
+module Rouge
+  module Lexers
+    class CerbKataLexer < RegexLexer
+      title "CerbKataLexer"
+      desc "Lexer for Cerb KATA"
+      tag 'cerb'
+      filenames '*.kata'
+
+      identifier = /[\w\-_.@&,\/]+/
+
+      state :basic do
+        rule %r/\s*#.*?\n/, Comment
+      end
+
+      state :root do
+        mixin :basic
+
+        rule %r/(\s*)(#{identifier}:)( *)(.*?)(\n|$)/ do |m|
+            groups Text, Keyword, Text, Str, Text
+            @parent_indent = m[1].length
+            push :value if m[2].include? '@' and 0 == m[4].length
+        end
+      end
+
+      state :value do
+        rule %r/(\s+)/ do |m|
+            token Text
+            if m[1].length <= @parent_indent
+                pop!; reset_stack; push :root
+            end
+        end
+
+        rule %r/(.*?)(\n|$)/ do |m|
+            groups Str, Text
+        end
+      end
+    end
+  end
+end
