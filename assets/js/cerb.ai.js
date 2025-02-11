9eb82539d81867b32ba602c6a007bf3828db89c3
Jeff Standen <jeff@webgroupmedia.com>
2025-02-10 16:52:19 -0800

    [Code/Syntax] Added back a 'Copy' button to code blocks.

---
+++ assets/js/cerb.ai.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function () {
+    // Credit: https://remarkablemark.org/blog/2021/06/01/add-copy-code-to-clipboard-button-to-jeyll-site/
+    let codeBlocks = document.querySelectorAll('figure.highlight');
+    
+    codeBlocks.forEach(function (codeBlock) {
+        let copyButton = document.createElement('button');
+        copyButton.className = 'copy';
+        copyButton.type = 'button';
+        copyButton.ariaLabel = 'Copy code to clipboard';
+        copyButton.innerText = 'Copy';
+        
+        codeBlock.append(copyButton);
+        
+        copyButton.addEventListener('click', function () {
+            let code = codeBlock.querySelector('code').innerText.trim();
+            window.navigator.clipboard.writeText(code);
+            
+            copyButton.innerText = 'Copied';
+            let fourSeconds = 4000;
+            
+            setTimeout(function () {
+                copyButton.innerText = 'Copy';
+            }, fourSeconds);
+        });
+    });
+});
